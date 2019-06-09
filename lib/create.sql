e3a54e08c21bf111f6f064b0e1682a00f4992883
Brooke Jones <bjones782@gmail.com>
2019-06-09 11:36:10 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
+
+-- INSERT INTO bears (d, name, age, gender, color, temperament, alive) VALUES ()
