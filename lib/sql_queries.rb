ba0d7c43f2c8fe65ebacfb4cfcf7addcee51fdb9
github username <github email address>
2019-06-10 16:35:14 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT * FROM bears gender"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -28,4 +28,5 @@ end
 
 def selects_bear_that_killed_Tim
   "Write your SQL query here"
+end
 end
